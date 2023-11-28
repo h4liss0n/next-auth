970bf3c00c0baf2a5b6e10116f6731530c31487c
Halisson Skalee <47496754+h4liss0n@users.noreply.github.com>
2023-11-28 10:34:26 +0000

    add login test

--- next-auth/__tests__/index.test.tsx
+++ next-auth/__tests__/index.test.tsx
@@ -1,15 +1,31 @@
-import LoginForm from '@/components/login/LoginForm'
-import { render, screen } from '@testing-library/react'
-import "@testing-library/jest-dom";
+import userEvent from '@testing-library/user-event';
+import LoginForm from '@/components/login/LoginForm';
+import { render, screen, waitFor } from '@testing-library/react';
+import { signIn } from 'next-auth/react';
+import '@testing-library/jest-dom';
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<LoginForm />)
 
-    const heading = screen.getByRole('textbox', {
-      name: /email/i,
-    })
+jest.mock('next-auth/react');
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+// afterEach(() => {
+//   jest.restoreAllMocks();  
+// });
+
+describe('LoginForm', () => {
+  it('fills in email and password inputs using user-events', async () => {
+    render(<LoginForm />);
+    const emailInput = screen.getByRole('textbox', { name: /email/i });
+    const passwordInput = screen.getByLabelText(/password/i);
+    const buttonSignin = screen.getByRole('button', { name: /sign in/i });
+
+    
+    await userEvent.type(emailInput, 'test@example.com');
+    await userEvent.type(passwordInput, 'testpassword');
+    await userEvent.click(buttonSignin);
+
+    
+    expect(emailInput).toHaveValue('test@example.com');
+    expect(passwordInput).toHaveValue('testpassword');
+    await waitFor(() => expect(signIn).toHaveBeenCalled());
+  });
+});
